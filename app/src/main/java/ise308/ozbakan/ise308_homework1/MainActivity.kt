113a229f95fc5dc0cb21909bf2b5b4c4476688b9
kutluhanozbakan <kutluhanozbakan21@gmail.com>
2020-10-28 11:18:45 +0300

    HOMEWORK1 - VAHİT KUTLUHAN ÖZBAKAN - 17244710056

--- app/src/main/java/ise308/ozbakan/ise308_homework1/MainActivity.kt
+++ app/src/main/java/ise308/ozbakan/ise308_homework1/MainActivity.kt
@@ -2,6 +2,7 @@ package ise308.ozbakan.ise308_homework1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.main_layout.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
